3f97741dbb2451258a6943746ad1c7eb34d07209
DanielPanutsa <german.poleshchuk@ya.ru>
2021-11-01 11:27:19 +0200

    Hover

--- js/script.js
+++ js/script.js
@@ -85,6 +85,16 @@ function text() {
 
 // Hover Event на учителях 
 
+function widthTest(){
+	if ($(window).width() < 375) {
+		return 0;
+	} else {
+		getLinkTeacher('teacher1','social1','teacher_image1','teacher_name1');
+		getLinkTeacher('teacher2','social2','teacher_image2','teacher_name2');
+		getLinkTeacher('teacher3','social3','teacher_image3','teacher_name3');
+		getLinkTeacher('teacher4','social4','teacher_image4','teacher_name4');	}
+}
+
 function getLinkTeacher(i,x,y,z){
 	document.getElementById(i).onmouseenter = function(){
 		document.getElementById(x).classList.add('social_opacity');
@@ -98,10 +108,10 @@ function getLinkTeacher(i,x,y,z){
 	}
 }
 
-getLinkTeacher('teacher1','social1','teacher_image1','teacher_name1');
-getLinkTeacher('teacher2','social2','teacher_image2','teacher_name2');
-getLinkTeacher('teacher3','social3','teacher_image3','teacher_name3');
-getLinkTeacher('teacher4','social4','teacher_image4','teacher_name4');
+//getLinkTeacher('teacher1','social1','teacher_image1','teacher_name1');
+//getLinkTeacher('teacher2','social2','teacher_image2','teacher_name2');
+//getLinkTeacher('teacher3','social3','teacher_image3','teacher_name3');
+//getLinkTeacher('teacher4','social4','teacher_image4','teacher_name4');
 //getLinkTeacher('teacher5','social5','teacher_image5','teacher_name5');
 //getLinkTeacher('teacher6','social6','teacher_image6','teacher_name6');
 //getLinkTeacher('teacher7','social7','teacher_image7','teacher_name7');
